f06d07cf1ae441b86349ea37e0072168f017b364
Jungzl <13jungzl@gmail.com>
2023-03-05 19:41:23 +0800

    chore: init

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param a
+ * @param b
+ */
+export function add<T extends number>(a: T, b: T) {
+  return a + b
+}
+
+/**
+ *
+ * @param a
+ * @param b
+ */
+export function minus<T extends number>(a: T, b: T) {
+  return a - b
+}
